28805737f9d4f31a51e37a674ece1a2c9f862b52
amallem <anirudh.mallem@rutgers.edu>
2015-11-13 17:09:11 -0500

    Merge conflict

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,22 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table language (
+  repo_name                 varchar(255) not null,
+  repo_lang                 varchar(255),
+  constraint pk_language primary key (repo_name))
+;
+
+
+
+
+# --- !Downs
+
+SET FOREIGN_KEY_CHECKS=0;
+
+drop table language;
+
+SET FOREIGN_KEY_CHECKS=1;
+
